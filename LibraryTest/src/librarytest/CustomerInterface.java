cdf9418d1b7ff08e7ab5f43754d573819c5c86bd
Hamza Hussain <wct8670@autuni.ac.nz>
2023-10-12 14:05:10 +1300

    FewOptions First Commit
    Added a new class called CustomerInterface

---
+++ LibraryTest/src/librarytest/CustomerInterface.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package librarytest;
+
+/**
+ *
+ * @author Hamza
+ */
+public class CustomerInterface {
+    public static void main(String[] args) {
+        LibraryContents lib = new LibraryContents();
+        System.out.println("Welcome to the Library");
+        Book mybook = new Book();
+        mybook.setAuthor("Obama");
+        mybook.setPage(230);
+        mybook.setTitle("My Life");
+        lib.addBook(mybook);
+    }
+    
+}
